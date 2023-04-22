96466a1487d576f465162ea4cb9c543694799e9c
Nikita Dovgal <nikitadovgalkubsau@yandex.ru>
2023-04-22 16:50:55 +0300

    Lab8_1

--- lib/main.dart
+++ lib/main.dart
@@ -1,87 +1,108 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'dart:math';
+import 'package:http/http.dart' as http;
 
-/**
- *
-<<<<<<< HEAD
- * Черновик для мерджа с ветками
- * здесь проходит вся основная работа над лабами/курсовой
-=======
- * Старт 7 лабы
->>>>>>> draft
- *
- */
+Future<List<Photo>> fetchPhotos(http.Client client) async {
+  final response = await client.get(Uri.parse('https://jsonplaceholder.typicode.com/photos'));
+  return compute(parsePhotos, response.body);
+}
 
-class MainScreen extends StatelessWidget{
-  const MainScreen({Key? key}) : super(key:key);
+List<Photo> parsePhotos(String responseBody) {
+  final parsed = jsonDecode(responseBody).cast<Map<String, dynamic>>();
+  return parsed.map<Photo>((json) => Photo.fromJson(json)).toList();
+}
+
+class Photo {
+  final int albumId;
+  final int id;
+  final String title;
+  final String url;
+  final String thumbnailUrl;
+
+  const Photo({
+    required this.albumId,
+    required this.id,
+    required this.title,
+    required this.url,
+    required this.thumbnailUrl,
+  });
+
+  factory Photo.fromJson(Map<String, dynamic> json) {
+    return Photo(
+      albumId: json['albumId'] as int,
+      id: json['id'] as int,
+      title: json['title'] as String,
+      url: json['url'] as String,
+      thumbnailUrl: json['thumbnailUrl'] as String,
+    );
+  }
+}
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
+    const appTitle = 'Text';
+
+    return const MaterialApp(
+      title: appTitle,
+      home: MyHomePage(title: appTitle),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Гавное окно")),
-      body: Center(child: ElevatedButton(
-        onPressed: (){
-          Navigator.push(context,MaterialPageRoute(builder: (context) => const SecondScreen()));
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: FutureBuilder<List<Photo>>(
+        future: fetchPhotos(http.Client()),
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return const Center(
+              child: Text('Text!'),
+            );
+          } else if (snapshot.hasData) {
+            return PhotosList(photos: snapshot.data!);
+          } else {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
         },
-        child:const Text("Выбрать")
-      )
       ),
     );
   }
 }
 
+class PhotosList extends StatelessWidget {
+  const PhotosList({Key? key, required this.photos}) : super(key: key);
 
-class SecondScreen extends StatelessWidget{
-  const SecondScreen({Key? key}) : super(key:key);
+  final List<Photo> photos;
 
   @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar: AppBar(title: const Text("Второе окно")),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Container(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children:[
-                ElevatedButton(
-                    onPressed: (){
-                      const snackBar = SnackBar(
-                        content: Text('Вы выбрали овтет "Да"'),
-                      );
-                      Navigator.pop(context);
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    },
-                    child:const Text("Да")
-                ),
-                ElevatedButton(
-                    onPressed: (){
-                      const snackBar = SnackBar(
-                        content: Text('Вы выбрали овтет "Нет"'),
-                      );
-                      Navigator.pop(context);
-                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                    },
-                    child:const Text("Нет")
-                )
-              ]
-            )
-          ),
-        ]
-      )
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 2,
+      ),
+      itemCount: photos.length,
+      itemBuilder: (context, index) {
+        return Image.network(photos[index].thumbnailUrl);
+      },
     );
   }
 }
-
-void main(){
-  runApp(MaterialApp(
-      initialRoute: '/',
-    routes: {
-      '/': (BuildContext context) => const MainScreen(),
-      '/second': (BuildContext context) => const SecondScreen()
-    }
-  ));
-}
-
-
